dd7dff50e253f29e7442bef3a15d3618232f54f6
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-12-01 17:33:12 +0900

    Initial commit

---
+++ src/test.js
@@ -0,0 +1,14 @@
+// const title1 = document.getElementById("title");
+// console.log(title1);
+
+// const title2 = document.querySelector("#title");
+// console.log(title2);
+
+// const containers = document.getElementsByClassName("container");
+// console.log(containers);
+
+// const buttonEl = document.createElement("button")
+// buttonEl.textContent = "ボタン";
+
+// const div1El = document.querySelector(".container");
+// div1El.appendChild(buttonEl);
